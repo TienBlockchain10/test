31b3ba7b2626867f3f418291f41cf150bed25454
TienBlockchain10 <nguyensecure@proton.me>
2024-02-16 13:34:34 -0600

    added the right words

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('noButton').addEventListener('click', function() {
     const noButton = document.getElementById('noButton');
     yesButton.style.fontSize = (parseInt(window.getComputedStyle(yesButton).fontSize) + 25) + 'px';
     yesButton.style.padding = (parseInt(window.getComputedStyle(yesButton).padding) + 25) + 'px';
-    const noButtonTexts = ["Really?", "Are you sure?", "Think again!", "But why?", "Please?"];
+    const noButtonTexts = ["No", "Are you sure LARA?", "Really sure LARA?", "Are you positive LARA???", "Honey please", "Just think about it bebe", "If you say no, I'll be muy sad", "I'll be muy muy sad", "I'll be muy muy muy sad", "I'll be uber sad", "Ok fine, I'll stop asking...", "Just kidding, PLEASE SAY YES", "Don't be ghey woman click yes naur", "You're breaking my heart ;("];
     const currentText = noButton.textContent;
     const currentIndex = noButtonTexts.indexOf(currentText);
     noButton.textContent = noButtonTexts[(currentIndex + 1) % noButtonTexts.length] || "No";
